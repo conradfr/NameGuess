0d7f2eae7d5e8e106c8f7895a736f81c1a790986
lcocatrix <lionel.c@meero.com>
2019-12-06 16:01:36 +0100

    Misc cleanup

--- lib/nameGuess_web/plugs/space.ex
+++ lib/nameGuess_web/plugs/space.ex
@@ -3,30 +3,19 @@ defmodule NameGuessWeb.Plugs.Space do
   alias NameGuess.Repo
   alias NameGuess.Space
 
-  @default_space_codename "paris"
-
   def init(options), do: options
 
   def call(conn, _opts) do
+    default_space_codename = Application.get_env(:nameGuess, :default_space_codename)
+
     space =
       conn.params
-      |> Map.get("space", @default_space_codename)
+      |> Map.get("space", default_space_codename)
       |> (&Repo.get_by(Space, codename: &1)).()
 
     unless space == nil do
-      # todo move to helper ?
-
-      ip = conn.remote_ip |> :inet_parse.ntoa() |> to_string()
-
-      if ip == "::ffff:172.16.202.136" do
-        space2 = Repo.get_by(Space, codename: "test")
-
-        assign(conn, :space, space2)
-        |> put_session(:space, space2)
-      else
-        assign(conn, :space, space)
-        |> put_session(:space, space)
-      end
+      assign(conn, :space, space)
+      |> put_session(:space, space)
     else
       conn
       |> Phoenix.Controller.render(NameGuessWeb.ErrorView, :"404")
